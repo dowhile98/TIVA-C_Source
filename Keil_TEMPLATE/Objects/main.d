a3b5f4774042e9166758d31c862c708563ec4802
QUINO B. JEFFRY <47931397+dowhile98@users.noreply.github.com>
2021-08-16 23:08:32 -0500

    CLASE 1

--- Keil_TEMPLATE/Objects/main.d
+++ Keil_TEMPLATE/Objects/main.d
@@ -1,8 +1,9 @@
 .\objects\main.o: main.c
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: C:\ti\TivaWare_C_Series-2.2.0.295\inc/tm4c123gh6pm.h
 .\objects\main.o: C:\Users\Lenovo\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123.h
 .\objects\main.o: C:\Users\Lenovo\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123GH6PM.h
 .\objects\main.o: C:\Users\Lenovo\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm4.h
-.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\main.o: C:\Users\Lenovo\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h
 .\objects\main.o: C:\Users\Lenovo\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h
 .\objects\main.o: C:\Users\Lenovo\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armcc.h
